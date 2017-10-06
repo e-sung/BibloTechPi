6254896ac98c186e6793d149c5a2371f2d65797b
esung <dev.esung@gmail.com>
2017-10-07 00:11:51 +0900

    callback -> promise; except login & signup queries

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var mysql = require('mysql');
 var validation = require('./src/validation.js');
 var changeUserInfo = require('./src/changeUserInfo.js');
 var signup = require('./src/signup.js');
-var connection = require('./src/db.js');
+var db = require('./src/db.js');
 
 //app settings
 app.use(nodeadmin(app));
@@ -93,116 +93,58 @@ app.post('/login',(req,res)=>{
 app.get('/user-info',(req,res)=>{
 	var email = req.query.email;
 	var sql = mysql.format("select * from user where email = ? ", email);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-
-		res.json(rows[0]);
-	})
+	db.sendQueryResultWith(sql,res,true);
 });
 
 app.get('/search-result/by-title',(req,res)=>{
 	var bookTitle  = req.query.value;
 	var sql = "select * from books where title" +  " like " + "'%" + bookTitle + "%'";
 	sql = mysql.format(sql);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-
-		res.json(rows);
-	});
+	db.sendQueryResultWith(sql,res);
 });
 
 app.get('/search-result/by-author',(req,res)=>{
 	var author  = req.query.value;
 	var sql = "select * from books where author " +  " like " + "'%" + author + "%'";
 	sql = mysql.format(sql);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		res.json(rows);
-	});
+	db.sendQueryResultWith(sql,res);
 });
 
 app.get('/search-result/by-publisher',(req,res)=>{
 	var publisher  = req.query.value;
 	var sql = "select * from books where publisher " +  " like " + "'%" + publisher + "%'";
 	sql = mysql.format(sql);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		res.json(rows);
-	});
+	db.sendQueryResultWith(sql,res);
 });
-
-
 
 app.get('/post-info/by-book-title',(req,res)=>{
 	var bookTitle = req.query.bookTitle;
 	var sql = mysql.format("select id, bookTitle, postTitle, writtenTime, writer from posts where bookTitle = ?",bookTitle);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		else{
-			res.json(rows);
-		}
-	});
+	db.sendQueryResultWith(sql,res);
 });
 
 app.get('/post-info/by-writer',(req,res)=>{
 	var writer = req.query.writer;
 	var sql = mysql.format("select id, bookTitle, postTitle, writtenTime, writer from posts where writer  = ?",writer);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		else{
-			res.json(rows);
-		}
-	});
+	db.sendQueryResultWith(sql,res);
 });
 
 app.get('/post-content',(req,res)=>{
 	var postId = req.query.postId;
 	var sql = mysql.format("select * from posts where id = ?",postId);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		else{
-			res.json(rows[0]);
-		}
-	});
+	db.sendQueryResultWith(sql,res,true);
 });
 
 app.get('/books/rented',(req,res)=>{
 	var renterEmail = req.query.email;
 	var sql = mysql.format("select * from books where renter_email = ?",renterEmail);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}else{
-			res.json(rows);
-		}
-	});
+	db.sendQueryResultWith(sql,res);
 });
 
 app.get('/books/read',(req,res)=>{
 	var email  = req.query.email;
 	var sql = mysql.format("select readBooks from user where email = ?",email);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}
-		else{
-			res.json(rows[0]);
-		}
-	});
+	db.sendQueryResultWith(sql,res,true);
 });
 
 app.post("/new-post-entry",(req,res)=>{
@@ -213,13 +155,7 @@ app.post("/new-post-entry",(req,res)=>{
 	var sql = "INSERT INTO posts (bookTitle,postTitle, postContent, writtenTime, writer) VALUES (?,?,?,NOW(),?) ";
 	var inserts = [bookTitle, postTitle, postContent, userName];	
 	sql = mysql.format(sql,inserts);
-	connection.query(sql,function(err,rows,fields){
-		if(err){
-			res.send("DB query error");
-		}else{
-			res.send("OK");
-		}
-	});
+	db.sendQueryResultStatusWith(sql,res)
 });
 
 app.patch("/written-post",(req,res)=>{
@@ -230,14 +166,7 @@ app.patch("/written-post",(req,res)=>{
 		var sql= "UPDATE posts set postTitle = ?, postContent = ? where id = ?";
 		var inserts = [postTitle, postContent, postId];
 		sql = mysql.format(sql,inserts);
-		connection.query(sql,function(err,rows,fields){
-			if(err){
-				res.send("DB Query error");
-			}
-			else{
-				res.send("OK");
-			}
-		});
+		db.sendQueryResultStatusWith(sql,res)
 	}
 	else{
 		res.send("authentication failed");
@@ -247,9 +176,7 @@ app.patch("/written-post",(req,res)=>{
 app.get('/rental-info',(req,res)=>{
 	var bookId = req.query.id;
 	var sql = mysql.format("select * from books where id = ?",bookId);
-	connection.query(sql,function(err,rows,fields){
-		res.send(rows[0]);	
-	});
+	db.sendQueryResultWith(sql,res,true);
 });
 
 app.post('/rent',(req,res)=>{
@@ -258,28 +185,29 @@ app.post('/rent',(req,res)=>{
 
 	if(isAuthenticated(req)){
 		var sql = mysql.format("select is_rented from books where id  = ?", bookId);
-		connection.query(sql,function(err,rows,fields){
-			if(rows[0].is_rented===0){//if the books is not rented
-				var sql2 = mysql.format("update books set is_rented = 1, rented_date = now(), due_date = DATE_ADD(now(),INTERVAL 15 DAY), renter_email = ? where id = ?", 
+		console.log(sql)
+		db.query(sql).
+		then(function updateRentalStatusOf(books){
+			console.log(books)
+			if(books[0].is_rented === 0){
+				var sql2 = mysql.format("update books set is_rented = 1, rented_date = now(), due_date = DATE_ADD(now(),INTERVAL 15 DAY), renter_email = ? where id = ?",
 					[userEmail, bookId]);
-				connection.query(sql2,function(err2,rows2,fields2){//rent the book : due date is 15days from now
-					if(err){
-						res.send("DB query error");
-					}
-				});
-
-				var sql3 = mysql.format("update user set rentscore = rentscore + 10, rentableBooks = rentableBooks - 1 where email= ?", userEmail);
-				connection.query(sql3,function(err3,rows3,fields3){//As user rents the book, the amount of books he can rent decrement(rentableBooks -= 1)
-					if(err){
-						res.send("DB query error");
-					}
-				});													//Also rentscore is incremented to encourage user to rent more books
-				res.send("Book is rented alright");
-			}
-			else{
+				return db.query(sql2)
+			}else{
 				res.status(400).send("Can't rent rented book");
 			}
-		});
+		}).
+		then(function updateRentalScore(){
+			var sql3 = mysql.format("update user set rentscore = rentscore + 10, rentableBooks = rentableBooks - 1 where email= ?", userEmail);
+			return db.query(sql3)
+		}).
+		then(function sendAlrightMessage(){
+			res.send("Book is rented alright");
+		}).catch(function sendErrorMessage(err){
+			console.log(err);
+			res.send("DB query error");
+		})
+
 	}
 	else{
 		console.log("UnAuthorized access");
@@ -292,47 +220,49 @@ app.post('/rent',(req,res)=>{
 app.post("/return",(req,res)=>{
 	var bookId = req.body.id;
 	var userName = req.body.userName; 
-	var sql = mysql.format("select readBooks from user where username = ?", userName);
-	connection.query(sql,function(err,rows,fields){
-		var sql2 = mysql.format("select *  from books where id  = ?", bookId);
-		connection.query(sql2,function(err2,rows2,fields2){
-			var bookTitle = rows2[0].title;
-			var is_rented = rows2[0].is_rented;
-			if (is_rented===1) {//check if this book is already rented. IF it's not already rented, it shouldn't be 'returned!'
-				var sql3 = mysql.format("update books set is_rented = 0, renter_email = NULL, rented_date = NULL, due_date = NULL where id = ?",bookId);
-				connection.query(sql3,function(err3,rows3,fields3){
-					console.log(rows3);
-				});
-
-				var booksReadByUser = rows[0].readBooks.split(";");//every books that user ever read 
-				var alreadyRead = false;
-				for(var i=0, item; item=booksReadByUser[i]; i++){//if user already read this book, we shouldn't add this book to the list
-					if (item===bookTitle) {                      //but if user hasn't read this book, we will add this book to the list
-						alreadyRead = true;						 //so we are making 'alreadyRead' variable that knows if this book was read
-						break;
-					}
-				}
-				var sql4, inserts;
-				if(alreadyRead){//if user read this book already, just update rentable books and rentscore
-					sql4 = "update user set rentableBooks = rentableBooks + 1, rentscore = rentscore + 10 where username = ?";
-					inserts = [userName];
-				}else{//if user hasn't read htis book, update everything
-					sql4 = "update user set rentableBooks = rentableBooks + 1, rentscore = rentscore + 10, readBooks = concat(?,readBooks) where username = ?";
-					inserts = [bookTitle + ";", userName];					
-				}
-				sql4 = mysql.format(sql4, inserts);
-				connection.query(sql4,function(err4,rows4,fields4){
-					console.log(userName + " is rewarded alright");
-				});
-				res.status(200).send("Returned the book!");
+	var bookTitle;
+	var sql = mysql.format("select *  from books where id  = ?", bookId);
+	db.query(sql)
+	.then(function returnThe(book){
+		bookTitle = book[0].title;
+		if(book[0].is_rented === 1){
+			var sql2 =  mysql.format("update books set is_rented = 0, renter_email = NULL, rented_date = NULL, due_date = NULL where id = ?",bookId);
+			return db.query(sql2)
+		}else{
+			res.status(400).send("Can't return returned book");
+		}
+	})
+	.then(function queryUserInfo(){
+		res.status(200).send("Returned the book!");
+		var sql3 = mysql.format("select readBooks from user where username = ?", userName);
+		return db.query(sql3)
+	})
+	.then(function reward(user){
+		var booksReadByUser = user[0].readBooks.split(";");
+		var alreadyRead = false;
+		for(var i=0, item; item = booksReadByUser[i];i++){
+			if(item === bookTitle){
+				alreadyRead = true;
+				break;
 			}
-			else{
-				res.status(400).send("Can't return returned book");
-			}
-		});
-	});
-});
-
+		}
+		var sql4, inserts;
+		if(alreadyRead){//if user read this book already, just update rentable books and rentscore
+			sql4 = "update user set rentableBooks = rentableBooks + 1, rentscore = rentscore + 10 where username = ?";
+			inserts = [userName];
+		}else{//if user hasn't read htis book, update everything
+			sql4 = "update user set rentableBooks = rentableBooks + 1, rentscore = rentscore + 10, readBooks = concat(?,readBooks) where username = ?";
+			inserts = [bookTitle + ";", userName];					
+		}
+		sql4 = mysql.format(sql4, inserts);
+		return db.query(sql4)
+	})
+	.then(function showRewardResult(){
+		console.log("user is rewarded alrgit");
+	}).catch(function(error){
+		console.log(error)
+	})
+})
 
 function isAuthenticated(req){ 
 	return new Promise(function(resolve, reject){
@@ -340,14 +270,17 @@ function isAuthenticated(req){
 		var userEmail = authToken.split("|")[0];                     
 		var userHash = authToken.split("|")[1];                     
 		var sql = mysql.format("select password from user where email = ?",userEmail);
-		connection.query(sql,function(err,rows,fields){
-			if(userHash == rows[0].password){
+		db.query(sql).
+		then(function(result){
+			if(userHash==result[0].password){
 				resolve(true);
+			}else{
+				reject(false);
 			}
-			else{
-				reject();
-			}
-		});
+		},function(err){
+			res.send(err);
+			console.log(err);
+		})
 	});
 }
 
