98d69104e9aa7d404f5c2e3d58894bb288ada1e7
esung <dev.esung@gmail.com>
2017-10-07 00:11:08 +0900

    callback -> promise; except login & signup queries

--- src/db.js
+++ src/db.js
@@ -10,4 +10,40 @@ connection.connect(function(err) {
     if (err) throw err;
 });
 
-module.exports = connection;
+module.exports = {
+	connection : connection,
+	query : function(sql){
+		return new Promise(function(resolve,reject){
+			connection.query(sql,function(err,rows,fields){
+				if(err){
+					reject(err)
+				}else{
+					resolve(rows)
+				}
+			});
+		})
+	},
+	sendQueryResultStatusWith : function(sql,res){
+		this.query(sql).
+		then(function(){
+			res.send("OK")
+		},function(error){
+			console.log(error)
+			res.send("DB Query Error")
+		})
+	},
+	sendQueryResultWith : function(sql,res,shouldReturnSingleItem){
+		this.query(sql).
+		then(function(queryResult){
+			if(shouldReturnSingleItem){
+				res.json(queryResult[0]);
+			}else{
+				res.json(queryResult);
+			}
+		},function(err){
+			console.log(err)
+			res.send("DB query Error");
+		})
+	}
+}
+
